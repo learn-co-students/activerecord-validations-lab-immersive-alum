ec6383f8e2bf8ae61f1bf30fd68332679ff53c11
Andrew Cohn <acohn@gm.slc.edu>
2018-04-11 19:18:50 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :clickbait
+  
+  def clickbait
+    title =~ (/Won\'t Believe|Top [0-9]*|Secret|Guess/) ? true : errors.add(:title, "Must be clickbait")
+  end
 end
